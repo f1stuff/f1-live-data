dd50e1d1212f882c6df337b0586e477b2fe0477c
f1stuff <f1stuff23@proton.me>
2025-03-29 12:00:10 +0100

    Update drivers. Switch TSU and LAW

--- src/dataimporter/message_handler.py
+++ src/dataimporter/message_handler.py
@@ -7,12 +7,12 @@ log.setLevel(logging.INFO)
 
 D_LOOKUP = [[12, 'ANT', 'Mercedes', '#6CD3BF', 'SOLID'], [63, 'RUS', 'Mercedes', '#6CD3BF', 'DOT'],
             [44, 'HAM', 'Ferrari', '#F91536', 'DOT'], [16, 'LEC', 'Ferrari', '#F91536', 'SOLID'],
-            [1, 'VER', 'Red Bull', '#3671C6', 'SOLID'], [30, 'LAW', 'Red Bull', '#3671C6', 'DOT'],
+            [1, 'VER', 'Red Bull Racing', '#3671C6', 'SOLID'], [13, 'TSU', 'Red Bull Racing', '#3671C6', 'DOT'],
             [81, 'PIA', 'McLaren', '#F58020', 'DOT'], [4, 'NOR', 'McLaren', '#F58020', 'SOLID'],
             [14, 'ALO', 'Aston Martin', '#358C75', 'SOLID'], [18, 'STR', 'Aston Martin', '#358C75', 'DOT'],
             [10, 'GAS', 'Alpine', '#2293D1', 'SOLID'], [7, 'DOO', 'Alpine', '#2293D1', 'DOT'],
-            [22, 'TSU', 'VCARB', '#5E8FAA', 'SOLID'], [6, 'HAD', 'RB', '#5E8FAA', 'DOT'],
-            [31, 'OCO', 'Haas F1 Team', '#B6BABD', 'SOLID'], [87, 'BEA', 'Haas F1 Team', '#B6BABD', 'DOT'],
+            [30, 'LAW', 'Racing Bulls', '#5E8FAA', 'SOLID'], [6, 'HAD', 'Racing Bulls', '#5E8FAA', 'DOT'],
+            [31, 'OCO', 'Haas', '#B6BABD', 'SOLID'], [87, 'BEA', 'Haas', '#B6BABD', 'DOT'],
             [27, 'HUL', 'Kick Sauber', '#C92D4B', 'DOT'], [5, 'BOR', 'Kick Sauber', '#C92D4B', 'SOLID'],
             [55, 'SAI', 'Williams', '#37BEDD', 'DOT'], [23, 'ALB', 'Williams', '#37BEDD', 'SOLID']]
 
